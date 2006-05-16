e8dd1c359500d4456b791c874fb6e314a339cf45
whiteangel <whiteangel@05f0625b-0610-0410-a78a-99fb5fca624d>
2006-05-16 17:41:24 +0000

    Added the header comment.

--- trunk/jimmy/MSNTransaction.java
+++ trunk/jimmy/MSNTransaction.java
@@ -1,10 +1,23 @@
-/*
- * MSNTransaction.java
- *
- * Created on Petek, 2006, april 14, 13:49
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+/* JIMMY - Instant Mobile Messenger
+   Copyright (C) 2006  JIMMY Project
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ **********************************************************************
+ File: jimmy/MSNTransaction.java
+
+ Author(s): Zoran Mesec, Matevz Jekovec
  */
 
 package jimmy;
