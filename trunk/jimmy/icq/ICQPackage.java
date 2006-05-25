b6e5ff90a39590601d86f6cc63dabd2ea0bd4aac
sahel <sahel@05f0625b-0610-0410-a78a-99fb5fca624d>
2006-05-25 22:02:42 +0000

    Made javadoc comments and completed the methods i wrote.

--- trunk/jimmy/icq/ICQPackage.java
+++ trunk/jimmy/icq/ICQPackage.java
@@ -36,6 +36,7 @@ public class ICQPackage {
 	public static final short FLAP_HEADER_SIZE = 6;
 	public static final short SNAC_HEADER_SIZE = 10;
 	public static final short TLV_HEADER_SIZE = 4;
+	public static final short HEADER_SIZE = 20;
 	public static final short NO_SNAC_FLAP_INDEX = 10; //[0 SNACK+FLAP+TLV) = 20 -> 10 
 	public static final short TLV_INDEX = 16;
 	public static final short SNAC_INDEX = 6;
@@ -46,6 +47,29 @@ public class ICQPackage {
 	private byte[] pkg = null;			//The package in byte array
 	
 	
+	/**
+	 * Creates a new instance of this class
+	 *
+	 */
+	public ICQPackage(){
+		
+	}
+	
+	/**
+	 * Creates a new instance of this class and sets the size of the package in advance
+	 * 
+	 * @param data_size The size of the data you will send
+	 */
+	public ICQPackage(int data_size){
+		if(this.pkg == null)
+			this.pkg = new byte[data_size+ICQPackage.HEADER_SIZE];
+	}
+	
+	/**
+	 * Creates a new instance of this class and sets the package to p
+	 * 
+	 * @param p The entire package in byte[]
+	 */
 	public ICQPackage(byte[] p){
 		this.pkg = p;
 		this.ch = this.pkg[1];
@@ -56,26 +80,41 @@ public class ICQPackage {
 	}
 	
 	/**
+	 * Sets the content we want to send.
 	 * 
-	 * @param content
+	 * @param content the "message" we send (data)
 	 */
 	public void setContent(byte[] content){
 		
 		this.tlv_size = content.length;
 		
 		if(this.pkg == null){
-			this.pkg = new byte[content.length + ICQPackage.FLAP_HEADER_SIZE + ICQPackage.SNAC_HEADER_SIZE + ICQPackage.TLV_HEADER_SIZE];
+			this.pkg = new byte[content.length + ICQPackage.HEADER_SIZE];
 			for(int i = 0; i<content.length; i++){
-				this.pkg[ICQPackage.FLAP_HEADER_SIZE+ICQPackage.SNAC_HEADER_SIZE+ICQPackage.TLV_HEADER_SIZE+i] = content[i];
+				this.pkg[ICQPackage.HEADER_SIZE+i] = content[i];
+			}
+		}else{
+			for(int i = 0; i<content.length; i++){
+				this.pkg[ICQPackage.HEADER_SIZE+i] = content[i];
 			}
 		}
 	}
 	
+	/**
+	 * Return the package that we want to send or just interpret.
+	 * 
+	 * @return the package in byte array
+	 */
 	public byte[] getPackage(){
 		
 		return this.pkg;
 	}
 	
+	/**
+	 * Sets the FLAP package header.
+	 * 
+	 * @param seq The sequence number (0x0000 - 0x8000, normally in increases from package to package)
+	 */
 	public void setFlap(short seq){
 		byte[] a;
 		if(this.ch == 2){
@@ -100,6 +139,14 @@ public class ICQPackage {
 		
 	}
 	
+	/**
+	 * Sets the SNAC package header.
+	 * 
+	 * @param family Service family ID.
+	 * @param subtype Service subtype ID-
+	 * @param flags Sets the SNAC flags header attribute. bit1 should be enabled if there are more packages of the same reqid
+	 * @param reqid Snac reqid -> you should save some information on sent SNACs for some time, you need back information when you recieve a package.
+	 */
 	public void setSnac(int family, int subtype, int flags, int reqid){
 		byte[] a = Utils.shortToBytes(Utils.unsignShort(family),true);
 		this.pkg[6] = a[0];
@@ -111,8 +158,15 @@ public class ICQPackage {
 		this.pkg[10] = a[0];
 		this.pkg[11] = a[1];
 		a = Utils.intToBytes(reqid, true);
+		this.pkg[12] = a[0];
+		this.pkg[13] = a[1];
 	}
 	
+	/**
+	 * Sets the TLV package header.
+	 * 
+	 * @param type The data type stored in the content of the package.
+	 */
 	public void setTlv(int type){
 		byte[] b = Utils.shortToBytes(Utils.unsignShort(type), true);
 		this.pkg[16] = b[0];
@@ -122,17 +176,32 @@ public class ICQPackage {
 		this.pkg[19] = b[1];
 	}
 	
+	/**
+	 * Tells if a package is or is set to be of SNAC type -> FLAP channel property is 0x02
+	 * 
+	 * @return true if is or is set to be SNAC
+	 */
 	public boolean isSnack(){
-		if(this.ch == 2)
+		if(this.ch == 0x02)
 			return true;
 		else
 			return false;
 	}
 	
-	public void isSnack(boolean b){
-		this.ch = 2;
+	/**
+	 * Sets the FLAP channel to c.
+	 * 
+	 * @param c Desired FLAP channel
+	 */
+	public void setChannel(byte c){
+		this.ch = c;
 	}
 	
+	/**
+	 * Returns the size of the FLAP content
+	 * 
+	 * @return FLAP content size
+	 */
 	public int getFlapSize(){
 		return this.flap_size;
 	}
