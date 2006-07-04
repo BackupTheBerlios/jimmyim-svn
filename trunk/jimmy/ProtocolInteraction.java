61d20a758c6d4752bf3975049051151687bc0095
whiteangel <whiteangel@05f0625b-0610-0410-a78a-99fb5fca624d>
2006-07-04 18:36:44 +0000

    Added changeContactStatus().

--- trunk/jimmy/ProtocolInteraction.java
+++ trunk/jimmy/ProtocolInteraction.java
@@ -32,6 +32,7 @@ import java.util.Vector;
 public interface ProtocolInteraction {
 	public void addContact(Contact c);
 	public void addContacts(Vector c);
+	public void changeContactStatus(Contact c);
 	public void stopProtocol(Protocol p);
 	public void setProtocolStatus(Protocol p, byte status);
 	public void msgRecieved(ChatSession cs, Contact c, String msg);
