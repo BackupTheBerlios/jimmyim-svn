6b5f55b5b6e641eb4ecf2fb566bb8877ed122bca
whiteangel <whiteangel@05f0625b-0610-0410-a78a-99fb5fca624d>
2006-08-16 21:03:11 +0000

    Commented header.

--- trunk/jimmy/jabber/JabberParseXML.java
+++ trunk/jimmy/jabber/JabberParseXML.java
@@ -1,3 +1,25 @@
+/* JIMMY - Instant Mobile Messenger
+   Copyright (C) 2006  JIMMY Project
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ **********************************************************************
+ File: jimmy/JabberProtocolXML.java
+ Version: pre-alpha  Date: 2006/05/18
+ Author(s): Matevz Jekovec
+ */
+
 package jimmy.jabber;
 
 import jimmy.*;
