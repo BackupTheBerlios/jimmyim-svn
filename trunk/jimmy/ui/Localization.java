8fde07399ecb562e6251cc3313aa630e7d4c763f
stereo_sl <stereo_sl@05f0625b-0610-0410-a78a-99fb5fca624d>
2007-10-21 21:05:03 +0000

    - localization rewritten to support utf-8 encoding - slovenian translation .po file resaved

--- trunk/jimmy/ui/Localization.java
+++ trunk/jimmy/ui/Localization.java
@@ -26,6 +26,7 @@
 package jimmy.ui;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Hashtable;
 
 import javax.microedition.midlet.MIDlet;
@@ -61,109 +62,100 @@ public class Localization {
 	private final static int MSGID = 1;
 	private final static int IDLE = 0;
 	
-	public static void setTranslation(String trCode, MIDlet midlet) {
-		InputStream is = midlet.getClass().getResourceAsStream("po/" + trCode.substring(0,2) + ".po");	//open a file named the first two characters of the trCode and append a suffix .po
-		
-		try {	//try, if the file existed and was open
-			is.available();
-		} catch (Exception e) {
-			return;	//otherwise, leave and don't create a hashtable
-		}
-		
-		//file was successfully open, read the file and fill the hashtable
-		hTable = new Hashtable();
-		
-		try {
-			int curChar;
-			String curMsgId = new String();
-			String curMsgComment = new String();
-			String curMsgStr = new String();
-			String buffer = new String();
-			boolean reading = false;
-			boolean usedEscapeSeq = false;
-			int curStat = IDLE;
-			
-			while ((curChar=is.read()) != -1) {
-				String curStr = String.valueOf((char)curChar);
-				if ((!reading) && (curStr.compareTo("#")==0)) {
-					curStat = COMMENT;
-				}
-				
-				if (curStat==COMMENT && curStr.compareTo("\n")==0) {
-					curStat = IDLE;
-					buffer = "";
-				}
+	public static void setTranslation(String trCode, MIDlet midlet) { 
+            InputStream is = midlet.getClass().getResourceAsStream("po/" + trCode.substring(0,2) + ".po");	//open a file named the first two characters of the trCode and append a suffix .po
+            hTable = new Hashtable();
+            try {
+                is.available();
+                InputStreamReader stream = new InputStreamReader(is, "UTF-8");
+                String key = new String();
+                String value = new String();
+                StringBuffer line;
+                String tmpString;
+                int status;
+                while(true) {
+                    line = new StringBuffer();
+                    readLine(line,stream);  //throws exception
+                    tmpString = line.toString();
 
-				if (curStr=="\\") {	//escape sequence used
-					curChar = is.read();
-					curStr = curStr + String.valueOf(curChar);
-					if (curStr == "\\n")	//newline
-						curStr = "\n";
-					else if (curStr == "\\\"")	//double quotes
-						curStr = "\"";
-					else if (curStr == "\\t")	//tab
-						curStr = "\t";
-					else if (curStr == "\\\\")	//backslash
-						curStr = "\\";
-					
-					usedEscapeSeq = true;
-				}
-				
-				if (buffer.compareTo("msgid")==0) {
-					curStat = MSGID;
-					
-					if (curMsgStr.length()!=0) {
-						if (curMsgStr.indexOf("_:") != -1) {	//expression has comment
-							int idx1 = curMsgStr.indexOf("_:");
-							int idx2 = curMsgStr.indexOf("\n", idx1+2);
-							curMsgComment = curMsgStr.substring(idx1, idx2);
-							curMsgStr = curMsgStr.substring(idx2+1);
-						}
-							
-						hTable.put(getHash(curMsgId, curMsgComment), curMsgStr);
-					}
-					curMsgId = ""; curMsgComment = ""; curMsgStr = ""; buffer="";
-				}
+                    if(tmpString.startsWith("#")) status=COMMENT;
+                    else if(tmpString.startsWith("msgid")) status=MSGID;
+                    else if(tmpString.startsWith("msgstr")) status=MSGSTR;
+                    else status=IDLE;  
+                                             
+                    switch (status) {
+                        case MSGID: //message id
+                            key=parseMsgIdString(tmpString);
+                            break;
+                        case MSGSTR: //message string
+                            value=parseMsgStrString(tmpString);
+                            hTable.put(getHash(key.toString()), value.toString());
+                            break;
+                        case COMMENT: //comment
+                            
+                            break;
+                        case IDLE: //empty line
+                            
+                            break;
+                    }
 
-				if (buffer.compareTo("msgstr")==0)
-					curStat = MSGSTR;
-
-				if ((curStat==MSGID || curStat==MSGSTR) && curStr.compareTo("\"")==0) {	//reading msgid or msgstr value
-					if (reading && !usedEscapeSeq) {
-						if (curStat == MSGID)
-							curMsgId += buffer;
-						else if (curStat == MSGSTR)
-							curMsgStr += buffer;
-						
-						reading = false;
-						buffer = ""; curStr="";
-					} else if (!reading) {
-						buffer = ""; curStr="";
-						reading = true;
-					}
-					
-				}
-		
-				if (curStr.compareTo("\n")!=0 && curStr.compareTo("\t")!=0)
-					buffer = buffer.concat(curStr);
-				
-				usedEscapeSeq = false;
-			}
-			if (curMsgStr.length()!=0) {	//TODO: only copy of the 39 lines above
-				if (curMsgStr.indexOf("_:") != -1) {	//expression has comment
-					int idx1 = curMsgStr.indexOf("_:");
-					int idx2 = curMsgStr.indexOf("\n", idx1+2);
-					curMsgComment = curMsgStr.substring(idx1, idx2);
-					curMsgStr = curMsgStr.substring(idx2+1);
-				}
-				
-				hTable.put(getHash(curMsgId, curMsgComment), curMsgStr);
-			}
-		} catch (Exception e) {
-			System.out.println(e);
-		}
+                }
+            } catch (Exception e) {
+                   return;
+            }
 	}
+        
+        public static void readLine(StringBuffer line, InputStreamReader stream) throws Exception
+        {
+            char c;
+            int ch;
+            try {
+                while (true) {
+                      //c = (char) stream.read();
+                      ch=stream.read();
+                      //Read key until end of the line is reached
+                      if(ch==-1) throw new Exception();
+                      c=(char)ch;
+                      if (c == '\n'){
+                             break;
+                      }
+                      line.append(c);
+                }
+            } catch (Exception e) {
+                    //System.out.println("THROW EXCEPTION!!!");
+                   throw new Exception();
+                //return -1;
+                }
+            
+            //System.out.println("LINE:"+line);
+            
+ 
+            
+
+            /*while (true) {
+                try {
+                  c = (char) stream.read();
+                  if (c == '\n'){
+                         break;
+                  }
+
+                  //Read value until end of line
+                  value.append(c);
+                } catch (Exception e) {
+                    return 0;
+                }
+            }*/
+           
+        }
 	
+        private static String parseMsgIdString(String key) {
+            return key.substring(7,key.length()-1);
+        }
+        
+        private static String parseMsgStrString(String value) {
+            return value.substring(8,value.length()-1);
+        }
+        
 	private static Integer getHash(String in) {
 		return new Integer(in.hashCode());
 	}
